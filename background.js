e48df2395ec2d3f9563b80479723f6d1edaecbde
Yusuf <yusuf.baran.dgn05@gmail.com>
2026-06-27 21:40:50 +0300

    Update socket connection URL and maintain keep-alive

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 importScripts('socket.io.js');
 
-const SERVER_URL = "http://localhost:3000";
+const SERVER_URL = "http://https://co-watch-ogdz.onrender.com";
 let socket = null;
 
 // MV3 Service Worker'ı hayatta tutmak için (Keep-Alive) Ping
